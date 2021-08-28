fd449c7daa99b70e2c7d2b2470262f4acd19862f
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-28 13:25:58 +0500

    feat: setting-up Routing: ProtectedRouted & IsUserRedirect

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
+  const user = null;
+
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path="/signin">
-        <SignIn />
-      </Route>
-      <Route exact path="/signup">
-        <SignUp />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
