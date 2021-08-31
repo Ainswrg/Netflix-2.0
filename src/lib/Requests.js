f46bb41d5b152413ca27cae655eaef962271fe3b
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-31 20:47:33 +0500

    feat: setting-up request film and TV

--- src/lib/Requests.js
+++ src/lib/Requests.js
@@ -1,8 +1,8 @@
 // Typically we would store in {process.env.API_KEY}
 const API_KEY = '38ffc00a44d0dc17b34ba82e0252cbab';
 
-export const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+export const requestsFilms = {
+  fetchTrending: `/trending/movie/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
@@ -10,4 +10,15 @@ export const requests = {
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export const requestsTV = {
+  fetchTrending: `/trending/TV/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/tv/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionTV: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyTV: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorTV: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceTV: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
 };
